c4ff973c0236b752c322df6c6e86979089957346
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-02-06 23:10:28 -0300

    feat: add config swagger-gen for create swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { createSwaggerDocs } from './common/swagger/create-swagger-docs';
+
+const DOC_ROUTE = 'docs';
+const API_PREFIX = '';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const API_PORT = configService.get('SERVER_PORT');
+
+  await createSwaggerDocs(app);
+  await app.listen(API_PORT, async () => {
+    console.log(`RUNNING AT: ${await app.getUrl()}/${API_PREFIX}`);
+    console.log(`DOCS: ${await app.getUrl()}/${DOC_ROUTE}`);
+  });
 }
 bootstrap();
