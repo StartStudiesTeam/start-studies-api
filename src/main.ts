fe3430505d326c6a66c4b4e8a9d3f991981286f6
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-02-07 22:07:42 -0300

    feat: enchance bootstrap logic to onditionally create swagger docs

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,25 @@ import { ConfigService } from '@nestjs/config';
 import { createSwaggerDocs } from './common/swagger/create-swagger-docs';
 
 const DOC_ROUTE = 'docs';
-const API_PREFIX = '';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const API_PORT = configService.get('SERVER_PORT');
+  const NODE_ENV = configService.get('NODE_ENV');
 
-  await createSwaggerDocs(app);
-  await app.listen(API_PORT, async () => {
-    console.log(`RUNNING AT: ${await app.getUrl()}/${API_PREFIX}`);
-    console.log(`DOCS: ${await app.getUrl()}/${DOC_ROUTE}`);
+  const isDevelopment = NODE_ENV === 'development';
+
+  if (isDevelopment) {
+    await createSwaggerDocs(app);
+  }
+
+  await app.listen(API_PORT, '0.0.0.0', async () => {
+    console.log(`RUNNING AT: ${await app.getUrl()}/`);
+    if (isDevelopment) {
+      console.log(`DOCS: ${await app.getUrl()}/${DOC_ROUTE}`);
+    }
   });
 }
 bootstrap();
