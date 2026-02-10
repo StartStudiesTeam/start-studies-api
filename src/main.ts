717dc41fa32959c69356e7b1203643ee58b58829
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-02-09 22:36:59 -0300

    build: add new packages for prisma and class validator

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { createSwaggerDocs } from './common/swagger/create-swagger-docs';
+import { ValidationPipe } from '@nestjs/common';
 
 const DOC_ROUTE = 'docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const API_PORT = configService.get('SERVER_PORT');
   const NODE_ENV = configService.get('NODE_ENV');
