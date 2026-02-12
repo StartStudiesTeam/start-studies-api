7df77972fd7bdcc7b38cd8d65bc6f0177e1b1085
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-02-11 23:21:55 -0300

    feat: add database dependency and create user use case

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './infra/nestjs/controllers/users.controller';
-import { UsersService } from './application/services/users.service';
+import { CreateUserUseCase } from './application/usecases/create-user/create-user.usecase';
+import { DatabaseModule } from '@/src/infra/database/database.module';
+import { UserRepository } from './domain/repositories/user-repository';
+import { PrismaUserRepository } from '@/src/infra/database/prisma/users/prisma-user.repository';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    CreateUserUseCase,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+  ],
 })
 export class UsersModule {}
