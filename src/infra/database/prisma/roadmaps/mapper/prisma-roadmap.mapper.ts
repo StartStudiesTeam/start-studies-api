b1c467d1d73841f4bc24860b8433c225d68357e1
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-03-05 22:18:31 -0300

    feat(roadmaps): add prisma roadmap mapper and create repository implementation

---
+++ src/infra/database/prisma/roadmaps/mapper/prisma-roadmap.mapper.ts
@@ -0,0 +1,38 @@
+import { Prisma, Roadmap as PrismaRoadmap } from '@prisma/client';
+import { Roadmap } from '@/src/modules/roadmaps/domain/roadmap';
+import { RoadmapStatusEnum } from '@/src/modules/roadmaps/domain/enums/roadmap-status.enum';
+
+export class PrismaRoadmapMapper {
+  static toEntity(raw: PrismaRoadmap): Roadmap {
+    const roadmapOrError = Roadmap.create(
+      {
+        title: raw.title,
+        description: raw.description,
+        userId: raw.userId,
+        status: raw.status as RoadmapStatusEnum,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+        deletedAt: raw.deletedAt,
+      },
+      raw.id,
+    );
+
+    if (roadmapOrError.isLeft()) {
+      throw roadmapOrError.value;
+    }
+
+    return roadmapOrError.value;
+  }
+
+  static toPrisma(entity: Roadmap): Prisma.RoadmapUncheckedCreateInput {
+    return {
+      title: entity.title,
+      description: entity.description,
+      userId: entity.userId,
+      status: entity.status ?? RoadmapStatusEnum.DRAFT,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt ?? undefined,
+      deletedAt: entity.deletedAt ?? undefined,
+    };
+  }
+}
